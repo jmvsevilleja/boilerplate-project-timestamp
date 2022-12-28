8282d3f1290252fa67cbf0b6f91c5dafe745706d
Jess Mark Sevilleja <jess@clinica.dev>
2022-12-28 12:19:13 +0800

    added solution Timestamp Microservice

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,12 +19,29 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", function (req, res) {
+  const date = new Date();
+  res.json({unix: date.getTime(), utc: date.toUTCString()});
+});
 
+app.get("/api/:date", function (req, res) {
+  const date = new Date(req.params.date);
+  if (date.getTime()) {
+    res.json({unix: date.getTime(), utc: date.toUTCString()});
+    return;
+  }
+  const timestamp = new Date(Number(req.params.date));
+  if (timestamp.getTime()) {
+    res.json({unix: timestamp.getTime(), utc: timestamp.toUTCString()});
+    return;
+  }
+  res.json({error: "Invalid Date"});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
